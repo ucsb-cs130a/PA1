ee883cc6552249f2191b137fa047dda9efbe1e66
Zexi Huang <zexi_huang@cs.ucsb.edu>
2019-10-15 00:19:39 -0700

    Initial commit

--- Dictionary.h
+++ Dictionary.h
@@ -3,15 +3,21 @@ using namespace std;
 
 class Dictionary {
 public:
-    // Insert an input set of n keys to the dictionary.  Print out the the hash functions comprising the perfect hash and the number of trials needed to generate each hash function.
+    // Insert an input set of n keys to the dictionary. This method should print out the following information:
+    // 1. The hash functions comprising the perfect hash (both levels)
+    // 2. The sum of squares of the number of keys mapped to each bin of the first level hash function, and
+    // 3. The number of trials needed to generate each hash function.
     void bulkInsert(int n, string *keys);
 
-    // Insert a key to the dictionary. Handle collision with separate chaining. Print out if the insertion caused a collision.
+    // Insert a key to the dictionary.
+    // Print out whether the insertion caused a collision in the second level hash.
+    // Handle collision with separate chaining.
     void insert(string key);
 
-    // Remove a key from the dictionary.
+    // Remove a key from the dictionary, if it exists.
     void remove(string key);
 
-    // Return whether a key is found in the dictionary. Print the buckets accessed during the operation.
+    // Return whether a key is found in the dictionary.
+    // Print the buckets (both first and second level) accessed during the operation.
     bool find(string key);
 };
